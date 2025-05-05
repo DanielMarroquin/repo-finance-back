e4c0e9e74a1f5b8c39cd02657e69728ae2ca777b
Daniel Marroquin <dev.danielmarroquin25@gmail.com>
2025-05-05 13:50:10 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { createExpressServer } from "routing-controllers";
+import 'dotenv/config';
+
+let PORT = 3002;
+
+// creates express app, registers all controller routes and returns you express app instance
+const app = createExpressServer({
+  cors: {
+    origin: 'http://localhost:4200',
+    credentials: true,
+  },
+  routePrefix: "/bp", 
+  controllers: [
+    __dirname + "/controllers/*{.js,.ts}",
+  ],
+});
+
+
+// run express application on port 3000
+app.listen(PORT, () => {
+  console.log(`Servidor Iniciado`);
+  console.log(`Host: http://localhost:${PORT}`);
+  console.log(`Fecha/Hora: ${new Date().toLocaleString()}`);
+});
