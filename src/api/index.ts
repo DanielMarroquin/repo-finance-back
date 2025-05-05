98ad27d6ab81826cc33b44182d8648d047881459
Daniel Marroquin <dev.danielmarroquin25@gmail.com>
2025-05-05 14:56:09 -0500

    commit

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
 // api/index.ts
-import app from '../main'; // Importa tu app Express existente
+import app from '../../src/main'; // Importa tu app Express existente
 export default app; // Exporta para Vercel
